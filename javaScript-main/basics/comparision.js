c15316b840da3fb7208876a3a94731a5892522d3
Divyansh Vashishtth <divyanshvashishtth123@gmail.com>
2026-06-17 15:55:37 +0530

    add comparision.js

---
+++ javaScript-main/basics/comparision.js
@@ -0,0 +1,11 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+/*
+The reson for above null is that an equlatiy check (==) and comparisions >< >= <= works diffrentily.
+Comparisions convert null to a number, treating it as 0.
+That's why null >= 0 is true and null > 0 is false.
+*/
