c3ec50f92bcd55e76a4aef4230392c93f6659a64
Divyansh Vashishtth <divyanshvashishtth123@gmail.com>
2026-06-17 16:38:19 +0530

    add nums and math.js file

---
+++ javaScript-main/basics/nums and math.js
@@ -0,0 +1,36 @@
+const score = 400;
+
+const balance = new Number(1000);
+console.log(balance);
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.8966;
+
+console.log(otherNumber.toPrecision(5));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+const num1 = Number.MAX_VALUE;
+console.log(num1);
+
+const num2 = Number.MIN_VALUE;
+console.log(num2);
+
+// +++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(11.8));
+// console.log(Math.random());
+
+console.log(Math.floor((Math.random()*10) + 1 ));
+
+
+
+
+
